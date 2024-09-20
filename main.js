d5d2c3d889b2d4805f39435416d9addba932f7c8
feralesma <152739382+feralesma@users.noreply.github.com>
2024-09-20 20:58:58 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // var cantidad = 8     ===> var está en desuso
 let cantidad = document.getElementById('cantidad');
 let boton = document.getElementById('generar');
-let contrasena = document.getElementById('contrasena')
+let contraseña = document.getElementById('contrasena')
+let botonL = document.getElementById('limpiar')
+let mensaje = document.getElementById("aviso");
+
 
 const cadenaCaracteres='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
 
@@ -15,11 +18,16 @@ console.log(typeof contraseña);
 function generar(){
 
     let numeroDigitado = parseInt (cantidad.value);
-    //console.log(numeroDigitado);
+    console.log(numeroDigitado);
 
     // Condicionales
+    if (isNaN(numeroDigitado)) {
+        alert("Debes introducir un número de caracteres para la contraseña!!");
+        return;
+    }
     if(numeroDigitado < 8){
         alert("La cantidad de caracteres tiene que ser mayor o igual a 8!!");
+        return;
     }
 
     let password = '';
@@ -40,14 +48,39 @@ function generar(){
         password +=caracterAleatorio;
     }
 
+    validar(password)
     //console.log('password generada: '+ password);
-    contrasena.value= password;
+    contraseña.value= password;
 
 }
 
+function limpiar() {
 
+    contraseña.value = '';
+    cantidad.value = '';
+    mensaje.textContent = '';
 
+}
 
+function validar(password) {
 
+    const tieneMayuscula = /[A-Z]/.test(password);
+    const tieneMinuscula = /[a-z]/.test(password);
+    const tieneNumero = /[0-9]/.test(password);
+    const tieneEspecial = /[!@#$%^&*()]/.test(password);
 
+    if (tieneMayuscula && tieneMinuscula && tieneNumero && tieneEspecial) {
+        mensaje.textContent = "La contraseña es fuerte!!";
+        mensaje.style.color = "green";
+        mensaje.style.fontSize = "20px";
+        //return true;
+    } else {
+        mensaje.textContent = "CONTRASEÑA DÉBIL!!  Debe incluir mayúsculas, minúsculas, números y caracteres especiales.";
+        mensaje.style.color = "red";
+        mensaje.style.fontSize = "20px";
+        //return false;
+    }
 
+}
+
+// 20240920 19:35
